377a0f9b96df94683cc908d09537431cdf4b4f13
bcy <bcygsws@163.com>
2021-03-03 19:13:39 +0800

    node.js+express跨域的两种解决方案

--- app.js
+++ app.js
@@ -25,10 +25,33 @@
  * 请求返回的结果格式(本质是一个含参函数的调用)：
  * foo([{"id":1,"name":"三国演义","author":"罗贯中","category":"……}]);
  * 函数调用，拿到请求返回的结果
+ *
+ * 充分理解和识记数据接口跨域的两种方案：
+ * 1.在路由之前，app.use('*',(req,res,next) =>{
+ * 		res.header();
+ * })
+ * 2.使用cors模块实现跨域
+ *
+ *
  */
 const express = require('express');
 const app = express();
 const router = require('./router.js');
+const cors = require('cors');
+/* 
+ 第一种跨域解决方案：app.use(cors({origin:'',credentials:true}));
+ 对象可以传值也可以不传。不传则就是对所有请求跨域
+*/
+// a.对客户端所有请求跨域
+app.use(cors({ origin: 'http://localhost:8787', credentials: true }));
+// b.对于单个或者多个域名跨域
+/* 
+app.use(cors({
+  origin: ['http://127.0.0.1:3006', 'http://127.0.0.1:5500'],
+  credentials: true
+}))
+
+*/
 // 托管静态资源文件
 app.use('/www', express.static('public'));
 const bodyParser = require('body-parser');
@@ -36,6 +59,27 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 // 解析提交的json格式字符串
 // app.use(bodyParser.json());
+// 添加一个中间件函数，实现跨域请求，参考：https://www.cnblogs.com/pdcan/p/12201930.html
+// app.all('*', (req, res, next) => {
+// 	// 设置跨域域名的三种方式：
+// 	// 1.设置允许跨域的域名，'*'代表所有域名都可以跨域
+// 	// res.header('Access-Control-Allow-Origin', '*');
+// 	// 2.允许域名http://localhost:8787跨域
+// 	// res.header('Access-Control-Allow-Origin', 'http://localhost:8787');
+// 	// 3.如果是要设置多个域名的跨域，可以用数组来存放域名
+// 	let originList = ['http://localhost:8787'];
+// 	// 数组中是否包含某个元素。如果这个分支成立，说明从客户端发来请求的域名，req.headers.origin存在于数组元素之中
+// 	if (originList.includes(req.headers.origin.toLowerCase())) {
+// 		// req.headers是属性 req.headers.origin req.header()是方法，req.method是属性，表示请求方式
+// 		res.header('Access-Control-Allow-Origin', req.headers.origin);
+// 	}
+// 	// 允许的header类型
+// 	res.header('Access-Control-Allow-Headers', 'Content-type');
+// 	// 跨域允许的请求方式
+// 	res.header('Access-Control-Allow-Methods', 'PUT,DELETE,GET,POST,OPTIONS');
+// 	res.header('Access-Control-Max-Age', 172800); // 预请求缓存20天
+// 	next();
+// });
 app.use(router);
 const port = 3000;
 app.listen(port, () => {
